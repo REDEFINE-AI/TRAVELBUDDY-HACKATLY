1e246e1be98dd51c0769f565dfe60b52bc600e4d
rinto11804 <rinto11804@gmail.com>
2025-01-05 15:47:55 +0530

    preline setup

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,100 +2,84 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div>
+      <div
+        className="mt-2 bg-gray-100 border border-gray-200 text-sm text-gray-800 rounded-lg p-4 dark:bg-white/10 dark:border-white/20 dark:text-white"
+        role="alert"
+        tabIndex={-1}
+        aria-labelledby="hs-soft-color-dark-label"
+      >
+        <span id="hs-soft-color-dark-label" className="font-bold">
+          Dark
+        </span>{" "}
+        alert! You should check in on some of those fields below.
+      </div>
+      <div
+        className="mt-2 bg-gray-50 border border-gray-200 text-sm text-gray-600 rounded-lg p-4 dark:bg-white/10 dark:border-white/10 dark:text-neutral-400"
+        role="alert"
+        tabIndex={-1}
+        aria-labelledby="hs-soft-color-secondary-label"
+      >
+        <span id="hs-soft-color-secondary-label" className="font-bold">
+          Secondary
+        </span>{" "}
+        alert! You should check in on some of those fields below.
+      </div>
+      <div
+        className="mt-2 bg-blue-100 border border-blue-200 text-sm text-blue-800 rounded-lg p-4 dark:bg-blue-800/10 dark:border-blue-900 dark:text-blue-500"
+        role="alert"
+        tabIndex={-1}
+        aria-labelledby="hs-soft-color-info-label"
+      >
+        <span id="hs-soft-color-info-label" className="font-bold">
+          Info
+        </span>{" "}
+        alert! You should check in on some of those fields below.
+      </div>
+      <div
+        className="mt-2 bg-teal-100 border border-teal-200 text-sm text-teal-800 rounded-lg p-4 dark:bg-teal-800/10 dark:border-teal-900 dark:text-teal-500"
+        role="alert"
+        tabIndex={-1}
+        aria-labelledby="hs-soft-color-success-label"
+      >
+        <span id="hs-soft-color-success-label" className="font-bold">
+          Success
+        </span>{" "}
+        alert! You should check in on some of those fields below.
+      </div>
+      <div
+        className="mt-2 bg-red-100 border border-red-200 text-sm text-red-800 rounded-lg p-4 dark:bg-red-800/10 dark:border-red-900 dark:text-red-500"
+        role="alert"
+        tabIndex={-1}
+        aria-labelledby="hs-soft-color-danger-label"
+      >
+        <span id="hs-soft-color-danger-label" className="font-bold">
+          Danger
+        </span>{" "}
+        alert! You should check in on some of those fields below.
+      </div>
+      <div
+        className="mt-2 bg-yellow-100 border border-yellow-200 text-sm text-yellow-800 rounded-lg p-4 dark:bg-yellow-800/10 dark:border-yellow-900 dark:text-yellow-500"
+        role="alert"
+        tabIndex={-1}
+        aria-labelledby="hs-soft-color-warning-label"
+      >
+        <span id="hs-soft-color-warning-label" className="font-bold">
+          Warning
+        </span>{" "}
+        alert! You should check in on some of those fields below.
+      </div>
+      <div
+        className="mt-2 bg-white/10 border border-white/10 text-sm text-white rounded-lg p-4"
+        role="alert"
+        tabIndex={-1}
+        aria-labelledby="hs-soft-color-light-label"
+      >
+        <span id="hs-soft-color-light-label" className="font-bold">
+          Light
+        </span>{" "}
+        alert! You should check in on some of those fields below.
+      </div>
     </div>
   );
 }
