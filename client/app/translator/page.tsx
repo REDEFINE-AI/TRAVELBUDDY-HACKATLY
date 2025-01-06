0085e64b066b7ff8e5660aa192d6d06e9be3330c
rinto11804 <rinto11804@gmail.com>
2025-01-06 07:14:57 +0530

    made language tabs

--- client/app/translator/page.tsx
+++ client/app/translator/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Segment from "../components/Segment";
 
-function Translator() {
-  return <div>Translator</div>;
+export default function Page() {
+  return (
+    <>
+      <section className="w-full h-screen bg-white px-4 pt-4">
+        <div className="flex items-center justify-center">
+          <Segment tabValues={["English", "Spanish"]} />
+        </div>
+      </section>
+    </>
+  );
 }
-
-export default Translator;
