cbb87d508b7dbfd9a1c824ea4f91c2ceaacac92d
rinto11804 <rinto11804@gmail.com>
2025-02-16 21:08:51 +0530

    changed alembic props

--- server/alambic/env.py
+++ server/alambic/env.py
@@ -3,6 +3,7 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from app.db import Base
+import os
 
 from alembic import context
 
@@ -19,7 +20,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata
+target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
